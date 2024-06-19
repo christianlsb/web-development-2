c902b098e6cb699ac89583d16421e84812314a2f
christianlsb <chrislsb09@gmail.com>
2024-06-19 20:00:35 -0300

    Inicia lógica com o react-query

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { Header, ItemsHome } from "../../components";
 import api from "../../api";
+import { useQuery } from 'react-query'
+
 
 export function Home() {
-  const [getData, setData] = useState([]);
+  // const [getData, setData] = useState([]);
 
-  useEffect(() => {
-    api.get().then((response) => {
-      setData(response.data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   api.get().then((response) => {
+  //     setData(response.data);
+  //   });
+  // }, []);
+
+
+  const { isLoading, error, data } = useQuery('responseData', () =>
+  fetch('https://bymykel.github.io/CSGO-API/api/pt-BR/skins.json').then(res =>
+    res.json([])
+  )
+)
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.messages
+
+    console.log(data)
 
   return (
     <>
       <Header />
-      <ItemsHome items={getData} />
+      <ItemsHome items={data} />
     </>
   );
 }
