9422f3c8ee93f05fbae73122b99d6a6e9763980b
christianlsb <chrislsb09@gmail.com>
2024-06-25 10:32:16 -0300

    Remove react-query

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Header, ItemsHome } from "../../components";
 import api from "../../api";
-import { useQuery } from 'react-query'
 
 
 export function Home() {
-  // const [getData, setData] = useState([]);
+  const [getData, setData] = useState([]);
 
-  // useEffect(() => {
-  //   api.get().then((response) => {
-  //     setData(response.data);
-  //   });
-  // }, []);
-
-
-  const { isLoading, error, data } = useQuery('responseData', () =>
-  fetch('https://bymykel.github.io/CSGO-API/api/pt-BR/skins.json').then(res =>
-    res.json([])
-  )
-)
-
-  if (isLoading) return 'Loading...'
-
-  if (error) return 'An error has occurred: ' + error.messages
-
-    console.log(data)
+  useEffect(() => {
+    api.get().then((response) => {
+      setData(response.data);
+    });
+  }, []);
 
   return (
     <>
       <Header />
-      <ItemsHome items={data} />
+      <ItemsHome items={getData} />
     </>
   );
 }
