6b56f927be088f783980f400588587c7358a0017
natskoe <koehlernathy@gmail.com>
2024-06-25 11:27:06 -0300

    Footer html

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+export function Footer(){
+    return(
+        <footer>
+            <div className="footer-aboutus">
+                <h2>
+                    Sobre nós!
+                </h2>
+                <p>Somos uma empresa dedicada a trazer uma melhor experiência na busca e compra de skins relacionadas com Counter Strike 2.</p>
+            </div>
+            <div className="footer-links">
+                <h2>
+                    Links rápidos!
+                </h2>
+                <ul>
+                    <li>
+                        <a href="#home">Início</a>
+                        <a href="#products">Produtos</a>
+                        <a href="#contact">Contato</a>
+                        <a href="#about">Sobre</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
