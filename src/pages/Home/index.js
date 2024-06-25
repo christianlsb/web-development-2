93df9d6bde1ce02d3fae586db8f1ea6b6fe4a495
christianlsb <chrislsb09@gmail.com>
2024-06-25 14:12:19 -0300

    Remove links

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,15 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api";
-import Slider from "react-slick";
 import { ItemsHome } from "../../components";
 
 export function Home() {
-  const [getPistols, setPistols] = useState([]);
   const [getKnifes, setKnifes] = useState([]);
 
   useEffect(() => {
     api.get().then((response) => {
-      const pistols = response.data.filter(
-        (product) => product.category.name === "Pistolas"
-      );
       const knifes = response.data.filter(
         (product) => product.category.name === "Facas"
       );
-      setPistols(pistols);
       setKnifes(knifes);
     });
   }, []);
