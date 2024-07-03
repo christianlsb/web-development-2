25b0b6b1c2f3ef81bd4ff88d64657591d376887c
ClaudioRibeiro2 <claudio_ribeiro2@estudante.sc.senai.br>
2024-07-03 19:46:09 -0300

    a

--- src/components/CardSkin/index.js
+++ src/components/CardSkin/index.js
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 import "./styles.css";
+import { useState } from "react";
 
 export function CardSkin({ name, image, id, btnView }) {
   // criar condição botão visualização
+
+  const[myArray, setMyArray] = useState([]);
+
+  const addSkin = () => {
+    const newSkin = name;
+    const newArray = [...myArray, newSkin];
+
+    localStorage.setItem('skinName', JSON.stringify(newArray))
+  }
 
   return (
     <div className="container">
       <div className="content">
         <h3>{name}</h3>
         <img src={image} alt={name} />
-        <button className="add-cart">Adicionar ao carrinho</button>
+        <button onClick={addSkin} className="add-cart">Adicionar ao carrinho</button>
         {btnView && (
           <button className="view-details">
             <Link className="link-details" to={`/product/${id}`}>Visualizar o produto</Link>
