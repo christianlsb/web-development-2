375ba59c91c5feaae9091ead067257f2cfaf27ab
ClaudioRibeiro2 <claudio_ribeiro2@estudante.sc.senai.br>
2024-07-03 17:00:57 -0300

    Tirada a imagem q tava com problema

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,10 +8,6 @@ export function Home() {
                 <h1 className="title-h1">Bem vindo ao SkinsBonitinhas!</h1>
                 <h2 className="title-h2">Um website para navegar de forma fácil, pelas skins do jogo!</h2>
             </section>
-            <section className="home-section row">
-                <img className="home-img rotate" alt="Pistola Desert Eagle" src="https://d2d9vfag1luski.cloudfront.net/55095e26-e042-4d54-811b-b11e3c5fbee7-front@3x.webp" />
-                <img className="home-img " alt="Pistola Desert Eagle" src="https://d2d9vfag1luski.cloudfront.net/55095e26-e042-4d54-811b-b11e3c5fbee7-front@3x.webp" />
-            </section>
             <section className="home-section">
                 <h2 className="title-h2">Acesse nosso catalogo de skins para saber mais!</h2>
                 <div className="row">
